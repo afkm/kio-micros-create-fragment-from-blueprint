9f056eabb20ab140024c27ae1b61ac902d110f1c
sandbender3000 <christian@afkm.io>
2017-05-26 16:15:05 +0200

    fixed package.json

--- index.js
+++ index.js
@@ -24,54 +24,54 @@ var session = driver.session();
 
 var listener = require('seneca')()
     .use('seneca-amqp-transport')
-    .add('cmd:createFragment,fromBlueprint:*,asChildOf:*,title:*', function(message, done) {
+    .add('cmd:createFragment,fromBlueprint:*,asChildOf:*,title:*,isStartNode:*', function(message, done) {
 
-        // get node plus all (bi-directional) related nodes, except 'parent' nodes
-        var queryString = "MATCH (startNode:Blueprint {cuid:'" + message.fromBlueprint + "'}) -[relation]- (childNode) WHERE NOT (startNode)<-[:USES]-(childNode) RETURN startNode, childNode, relation ORDER BY relation.nodeOrder";
-        console.log(queryString);
+        // get node plus all (bi-directional) related nodes, except 'parent' and sibling 'blueprint'-generated nodes (instances)
+        var queryString = "MATCH (startNode {cuid:'" + message.fromBlueprint + "'}) -[relation]- (childNode) WHERE NOT (startNode)<-[:USES]-(childNode) AND NOT (startNode)<-[:BLUEPRINT_INSTANCE]-(childNode) RETURN startNode, childNode, relation ORDER BY relation.nodeOrder";
+        // console.log(queryString);
 
         session
             .run(queryString)
-
             .then(function(result) {
 
                 var newNodeCuid = null;
 
+                console.log("result set length: " + result.records.length);
+
                 async.eachSeries(result.records, function(record, callback) {
 
-                    console.log("newNodeCuid: " + newNodeCuid + ", record: " + result.records);
+                    console.log(`stepping into startNode ${record.get('startNode').properties.cuid} (${record.get('startNode').labels[0]}) -[${record.get('relation').properties[0]}]- childNode ${record.get('childNode').properties.cuid} (${record.get('childNode').labels[0]})`);
 
                     if (newNodeCuid == null) {
-                        // Create Fragment node
                         newNodeCuid = cuid();
-                        var msg = "cmd:addNode,cuid:" + newNodeCuid + ",nodeType:Fragment,nodeTitle:" + message.title + ",linkOut:" + message.asChildOf + ",linkType:USES,linkProps:{}";
-                        listener.act(msg, (err, result) => {
-                            if (err) {
-                                throw err;
-                            }
-                        });
-                        // Link Fragment to Blueprint node
-                        var msg = "cmd:linkNode,cuid:" + newNodeCuid + ",linkTo:" + message.fromBlueprint + ",linkType:BLUEPRINT_INSTANCE,linkProps:{}";
-                        listener.act(msg, (err, result) => {
-                            if (err) {
-                                throw err;
-                            }
-                        });
+                        if (message.isStartNode) {
+                            // Create Fragment from Blueprint node
+                            addNode(newNodeCuid, "Fragment", message.title, message.asChildOf);
+                            // Link Fragment to Blueprint node
+                            linkNode(newNodeCuid, message.fromBlueprint, "BLUEPRINT_INSTANCE");
+                        } else {
+                            // Create node from startNode
+                            addNode(newNodeCuid, record.get('startNode').labels[0], message.title, message.asChildOf);
+                        }
                     }
 
                     // Either set start node modifier...
                     if (record.get('childNode').labels.includes('Modifier')) {
-                        var msg = "cmd:linkNode,cuid:" + record.get('childNode').properties.cuid + ",linkTo:" + newNodeCuid + ",linkType:MODIFIES,linkProps:{}";
-                        listener.act(msg, (err, result) => {
-                            if (err) {
-                                throw err;
-                            }
-                        });
-                        return callback();
+                        linkNode(record.get('childNode').properties.cuid, newNodeCuid, "MODIFIES");
+                        callback();
 
-                        // ...or traverse deeper
+                    // ...or traverse deeper
                     } else {
-                        var msg = "cmd:createNode,fromNode:" + record.get('childNode').properties.cuid + ",asChildOf:" + newNodeCuid;
+
+                        console.log("Traversing " + record.get('childNode').properties.cuid);
+
+                        var childTitle;
+                        if (_.has(record.get('childNode').properties.title)) {
+                            childTitle = record.get('childNode').properties.title;
+                        } else {
+                            childTitle = "''";
+                        }
+                        var msg = "cmd:createFragment,fromBlueprint:" + record.get('childNode').properties.cuid + ",asChildOf:" + newNodeCuid + ",title:" + childTitle + ",isStartNode:false";
                         listener.act(msg, (err, result) => {
                             if (err) {
                                 throw err;
@@ -80,20 +80,16 @@ var listener = require('seneca')()
                         });
                     }
 
-
                 }, function(err) {
-
                     session.close();
-
                     if (err) {
                         console.error(err);
                         return;
                     }
-
-                    return done(null, newNodeCuid);
+                    var status = "Successfully created Fragment from Blueprint " + message.fromBlueprint;
+                    return done(null, { status });
                 });
             })
-
             .catch(function(error) {
                 console.log(error);
             });
@@ -101,9 +97,41 @@ var listener = require('seneca')()
 
     .listen({
         type: 'amqp',
-        pin: 'cmd:createFragment,fromBlueprint:*,asChildOf:*,title:*',
+        pin: 'cmd:createFragment,fromBlueprint:*,asChildOf:*,title:*,isStartNode:*',
         url: process.env.AMQP_URL
     });
+
+
+function addNode(cuid, type, title, linkTo) {
+    var queryString = "MERGE (" + cuid + ":" + type + " { cuid:'" + cuid + "', title:'" + title + "' })\n";
+    queryString += "WITH 1 as dummy\n";
+    queryString += "MATCH (a { cuid: '" + cuid + "' }), (b { cuid: '" + linkTo + "'}) MERGE (a)<-[:USES]-(b)\n";
+    // console.log(queryString);
+    session
+      .run(queryString)
+      .then(function(result) {
+        session.close();
+        console.log("Successfully added Node " + cuid);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+}
+
+function linkNode(cuid, linkTo, linkType) {
+    var queryString = "MATCH (a { cuid:'" + cuid + "'}), (b { cuid:'" + linkTo + "'}) MERGE (a)-[:" + linkType + "]->(b)";
+    // console.log(queryString);
+    session
+      .run(queryString)
+      .then(function(result) {
+        session.close();
+        console.log("Successfully linked Node " + cuid + " to Node " + linkTo);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+}
+
 
 // Will never be called when quitting service...
 driver.close();
