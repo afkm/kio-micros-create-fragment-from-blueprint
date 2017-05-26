80fe4a3f313b324ace5384f00b02e0563b7b6f54
sandbender3000 <christian@afkm.io>
2017-05-26 17:49:17 +0200

    working version

--- index.js
+++ index.js
@@ -28,67 +28,72 @@ var listener = require('seneca')()
 
         // get node plus all (bi-directional) related nodes, except 'parent' and sibling 'blueprint'-generated nodes (instances)
         var queryString = "MATCH (startNode {cuid:'" + message.fromBlueprint + "'}) -[relation]- (childNode) WHERE NOT (startNode)<-[:USES]-(childNode) AND NOT (startNode)<-[:BLUEPRINT_INSTANCE]-(childNode) RETURN startNode, childNode, relation ORDER BY relation.nodeOrder";
-        // console.log(queryString);
 
         session
             .run(queryString)
             .then(function(result) {
 
-                var newNodeCuid = null;
+                var startNodeCuid = null;
 
                 console.log("result set length: " + result.records.length);
 
                 async.eachSeries(result.records, function(record, callback) {
 
-                    console.log(`stepping into startNode ${record.get('startNode').properties.cuid} (${record.get('startNode').labels[0]}) -[${record.get('relation').properties[0]}]- childNode ${record.get('childNode').properties.cuid} (${record.get('childNode').labels[0]})`);
+                        console.log(`stepping into startNode ${record.get('startNode').properties.cuid} (${record.get('startNode').labels[0]}) -[${record.get('relation').properties[0]}]- childNode ${record.get('childNode').properties.cuid} (${record.get('childNode').labels[0]})`);
 
-                    if (newNodeCuid == null) {
-                        newNodeCuid = cuid();
-                        if (message.isStartNode) {
-                            // Create Fragment from Blueprint node
-                            addNode(newNodeCuid, "Fragment", message.title, message.asChildOf);
-                            // Link Fragment to Blueprint node
-                            linkNode(newNodeCuid, message.fromBlueprint, "BLUEPRINT_INSTANCE");
-                        } else {
-                            // Create node from startNode
-                            addNode(newNodeCuid, record.get('startNode').labels[0], message.title, message.asChildOf);
-                        }
-                    }
-
-                    // Either set start node modifier...
-                    if (record.get('childNode').labels.includes('Modifier')) {
-                        linkNode(record.get('childNode').properties.cuid, newNodeCuid, "MODIFIES");
-                        callback();
-
-                    // ...or traverse deeper
-                    } else {
-
-                        console.log("Traversing " + record.get('childNode').properties.cuid);
-
-                        var childTitle;
-                        if (_.has(record.get('childNode').properties.title)) {
-                            childTitle = record.get('childNode').properties.title;
-                        } else {
-                            childTitle = "''";
-                        }
-                        var msg = "cmd:createFragment,fromBlueprint:" + record.get('childNode').properties.cuid + ",asChildOf:" + newNodeCuid + ",title:" + childTitle + ",isStartNode:false";
-                        listener.act(msg, (err, result) => {
-                            if (err) {
-                                throw err;
+                        if (startNodeCuid == null) {
+                            if (message.isStartNode) {
+                                startNodeCuid = cuid();
+                                // Create Fragment from Blueprint node
+                                addNode(startNodeCuid, "Fragment", message.title, message.asChildOf);
+                                // Link Fragment to Blueprint node
+                                linkNode(startNodeCuid, message.fromBlueprint, "BLUEPRINT_INSTANCE");
+                            } else {
+                                startNodeCuid = message.asChildOf;
                             }
-                            return callback();
-                        });
-                    }
+                        }
 
-                }, function(err) {
-                    session.close();
-                    if (err) {
-                        console.error(err);
-                        return;
-                    }
-                    var status = "Successfully created Fragment from Blueprint " + message.fromBlueprint;
-                    return done(null, { status });
-                });
+                        // Either set start node modifier...
+                        if (record.get('childNode').labels.includes('Modifier')) {
+                            linkNode(record.get('childNode').properties.cuid, startNodeCuid, "MODIFIES");
+                            callback();
+                        }
+
+                        // ...or traverse deeper
+                        else {
+
+                            var childTitle = "";
+                            if (_.has(record.get('childNode').properties, "title")) {
+                                childTitle = _.get(record.get('childNode').properties, "title");
+                            }
+
+                            // Create node from childNode
+                            var newChildNodeCuid = cuid();
+                            addNode(newChildNodeCuid, record.get('childNode').labels[0], childTitle, startNodeCuid);
+
+                            console.log("Traversing " + record.get('childNode').properties.cuid);
+
+                            var msg = "cmd:createFragment,fromBlueprint:" + record.get('childNode').properties.cuid + ",asChildOf:" + newChildNodeCuid + ",title:'',isStartNode:false";
+                            listener.act(msg, (err, result) => {
+                                if (err) {
+                                    throw err;
+                                }
+                                return callback();
+                            });
+                        }
+
+                    },
+                    function(err) {
+                        session.close();
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                        var status = "Successfully created Fragment from Blueprint " + message.fromBlueprint;
+                        return done(null, {
+                            status
+                        });
+                    });
             })
             .catch(function(error) {
                 console.log(error);
@@ -106,30 +111,29 @@ function addNode(cuid, type, title, linkTo) {
     var queryString = "MERGE (" + cuid + ":" + type + " { cuid:'" + cuid + "', title:'" + title + "' })\n";
     queryString += "WITH 1 as dummy\n";
     queryString += "MATCH (a { cuid: '" + cuid + "' }), (b { cuid: '" + linkTo + "'}) MERGE (a)<-[:USES]-(b)\n";
-    // console.log(queryString);
+    console.log("addNode: " + queryString);
     session
-      .run(queryString)
-      .then(function(result) {
-        session.close();
-        console.log("Successfully added Node " + cuid);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+        .run(queryString)
+        .then(function(result) {
+            session.close();
+            console.log("Successfully added Node " + cuid);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
 
 function linkNode(cuid, linkTo, linkType) {
     var queryString = "MATCH (a { cuid:'" + cuid + "'}), (b { cuid:'" + linkTo + "'}) MERGE (a)-[:" + linkType + "]->(b)";
-    // console.log(queryString);
     session
-      .run(queryString)
-      .then(function(result) {
-        session.close();
-        console.log("Successfully linked Node " + cuid + " to Node " + linkTo);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+        .run(queryString)
+        .then(function(result) {
+            session.close();
+            console.log("Successfully linked Node " + cuid + " to Node " + linkTo);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
 
 
